059022c977d4739f992aa9e056275408cc5b695d
WellsSA <wel.cavzod@gmail.com>
2021-12-22 08:36:01 -0300

    feat: implementing ObjectMethodsService

--- src/service/ObjectMethodsService.js
+++ src/service/ObjectMethodsService.js
@@ -1,18 +1,41 @@
 class ObjectMethodsService {
   getEnhancedObject(rawObject) {
     // TODO: return an enhanced object implementing `valueOf` or `toString` depending on the context
-    return null;
+    const enhancedObject = {
+      ...rawObject,
+      valueOf() {
+        return rawObject.age;
+      },
+    };
+    return enhancedObject;
   }
 
   getEnhancedObject2(rawObject) {
     // TODO: return an enhanced object implementing `valueOf` or `toString` depending on the context
-    return null;
+    const enhancedObject = {
+      ...rawObject,
+      toString() {
+        return `[name="${rawObject.name}",age=${rawObject.age}]`;
+      },
+    };
+    return enhancedObject;
   }
 
   getEnhancedObjectWithoutValueOfOrToString(rawObject) {
     // TODO: return an enhanced object **WITHOUT** implementing `valueOf` nor `toString`.
     // Note: Implementing `toString` and `valueOf` will NOT be allowed in this function.
-    return null;
+    const enhancedObject = {
+      ...rawObject,
+      [Symbol.toPrimitive](coercionType) {
+        const types = {
+          string: `[name="${rawObject.name}",age=${rawObject.age}]`,
+          number: rawObject.age,
+        };
+
+        return types[coercionType] || types.string;
+      },
+    };
+    return enhancedObject;
   }
 }
 
