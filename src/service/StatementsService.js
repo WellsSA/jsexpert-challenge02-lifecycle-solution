3292a0237ba5b536d24b38863150ad35cd8a30ec
WellsSA <wel.cavzod@gmail.com>
2021-12-22 08:36:36 -0300

    feat: implementing StatementsService

--- src/service/StatementsService.js
+++ src/service/StatementsService.js
@@ -1,22 +1,22 @@
 class StatementsService {
   getReturnBasedOnStatement(condition1, condition2) {
     // TODO: use condition stations && or || in order to return the expected value
-    return null;
+    return condition1 || condition2;
   }
 
   getReturnBasedOnStatement2(condition1, condition2) {
     // TODO: use condition stations && or || in order to return the expected value
-    return null;
+    return condition1 && condition2;
   }
 
   getReturnBasedOnStatement3(condition1, condition2, condition3) {
     // TODO: use condition stations && or || in order to return the expected value (chaining all conditions)
-    return null;
+    return (condition1 && condition2) || condition3;
   }
 
   getReturnBasedOnStatement4(condition1, condition2, condition3) {
     // TODO: use condition stations && or || in order to return the expected value (chaining all conditions)
-    return null;
+    return (condition1 || condition2) && condition3;
   }
 }
 
