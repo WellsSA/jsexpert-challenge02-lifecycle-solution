48ca8e181791bd1515226e24f58442d3f1d89f35
WellsSA <wel.cavzod@gmail.com>
2021-12-22 08:36:22 -0300

    feat: implementing ReferencesService

--- src/service/ReferencesService.js
+++ src/service/ReferencesService.js
@@ -1,21 +1,27 @@
 class ReferencesService {
   getCounterCopyingReferences(object) {
     // TODO: assing object to a new variable and return it.
-    return null;
+    const newObject = object; // copying only the memory ref
+    return newObject;
   }
 
   getCounterWithoutCopyingReferences(object) {
     // TODO: assing object to a new variable and return it.
-    return null;
+    // Also viable: Object.create(object)
+    // Also viable: Object.assign({},object)
+    const newObject = { ...object }; // generating a new object based on the first one
+    return newObject;
   }
 
-  /*
-  // @TIP: if you want to do the extra sub-challenge, uncomment and implement the following function:
   getCounterWithoutDeepCopyingReferences(object) {
     // TODO: assing object to a new variable and return it.
-    return null;
+    // NodeJS 17+
+    // const newObject = structuredClone(object); // https://github.com/HalexV/jsexpert-challenge02-lifecycle/blob/50a58f7d884613d2acce9a25177f798931d6d9a4/src/service/ReferencesService.js#L17
+
+    // Also viable: _.deepClone(object) // using lodash
+    const newObject = JSON.parse(JSON.stringify(object)); // or use lodash, maybe?
+    return newObject;
   }
-  */
 }
 
 module.exports = ReferencesService;
